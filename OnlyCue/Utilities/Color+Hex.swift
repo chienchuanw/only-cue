ec2132dcd95cff5d11b2de7074a19d7dd316df43
chienchuanw <chienchuanwww@gmail.com>
2026-05-07 20:17:40 +0800

    fix(test): drop groupsByEvent=false and remove unused Color.hexString

--- OnlyCue/Utilities/Color+Hex.swift
+++ OnlyCue/Utilities/Color+Hex.swift
@@ -1,4 +1,3 @@
-import AppKit
 import SwiftUI
 
 extension Color {
@@ -15,15 +14,5 @@ extension Color {
         let green = Double((value >> 8) & 0xFF) / 255
         let blue = Double(value & 0xFF) / 255
         self.init(red: red, green: green, blue: blue)
-    }
-
-    var hexString: String? {
-        guard let components = NSColor(self).usingColorSpace(.sRGB)?.cgColor.components, components.count >= 3 else {
-            return nil
-        }
-        let red = Int((components[0] * 255).rounded())
-        let green = Int((components[1] * 255).rounded())
-        let blue = Int((components[2] * 255).rounded())
-        return String(format: "#%02X%02X%02X", red, green, blue)
     }
 }

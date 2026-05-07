ffd645b6b4f07a2ce4d0615d36f5372f50931412
chienchuanw <chienchuanwww@gmail.com>
2026-05-07 16:41:36 +0800

    test: replace C1 placeholders with DocumentLaunchTests covering BDD scenario

--- OnlyCueTests/OnlyCueTests.swift
+++
@@ -1,7 +0,0 @@
-import XCTest
-
-final class OnlyCueTests: XCTestCase {
-    func test_placeholder() {
-        XCTAssertTrue(true)
-    }
-}
--- OnlyCueUITests/OnlyCueUITests.swift
+++
@@ -1,7 +0,0 @@
-import XCTest
-
-final class OnlyCueUITests: XCTestCase {
-    func test_placeholder() {
-        XCTAssertTrue(true)
-    }
-}

8753971b02e4da925001308d9be5717a55d42659
Chuan (home) <chienchuanwww@gmail.com>
2026-08-17 12:41:42 +0800

    test(miniplayer): progress/length model + absolute-seek tests

--- OnlyCueTests/MiniPlaybackActionsTests.swift
+++ OnlyCueTests/MiniPlaybackActionsTests.swift
@@ -9,4 +9,20 @@ final class MiniPlaybackActionsTests: XCTestCase {
         XCTAssertEqual(MiniPlaybackActions.rateChange(for: .rateReset), .reset)
         XCTAssertNil(MiniPlaybackActions.rateChange(for: .playPause))
     }
+
+    // MARK: - Absolute seek (#758)
+
+    func test_seekTarget_mapsFractionToSeconds() {
+        XCTAssertEqual(MiniPlaybackActions.seekTarget(fraction: 0.5, duration: 200), 100)
+    }
+
+    func test_seekTarget_clampsOutOfRangeFractions() {
+        XCTAssertEqual(MiniPlaybackActions.seekTarget(fraction: -0.2, duration: 200), 0)
+        XCTAssertEqual(MiniPlaybackActions.seekTarget(fraction: 1.5, duration: 200), 200)
+    }
+
+    func test_seekTarget_nilForNonPositiveDuration() {
+        XCTAssertNil(MiniPlaybackActions.seekTarget(fraction: 0.5, duration: 0))
+        XCTAssertNil(MiniPlaybackActions.seekTarget(fraction: 0.5, duration: -10))
+    }
 }
